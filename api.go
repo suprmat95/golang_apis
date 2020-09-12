173b920c648d9b9ff04b2aedede86c2f9bb29113
Matteo D'Ospina <matteodospina@gmail.com>
2020-09-12 12:43:23 +0200

    APIs implemented

--- api.go
+++ api.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -15,6 +18,9 @@ type Book struct {
 	Author *Author `json:"author"`
 }
 
+// Init books var as a slice Book struct
+var books []Book
+
 //Author struct (Model)
 
 type Author struct {
@@ -24,36 +30,72 @@ type Author struct {
 
 // Get All Books
 func getBooks(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
 }
 
 // Get Single Book
 
 func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	// Loop through books and find id
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
 
 }
 
 // Create a new Book
 func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(10000000)) //Mock ID - not safe
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
 
 }
 
 // Update a Book
 func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			books[index] = book
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
 
 }
 
 // Delete Book
 func deleteBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+		}
+	}
 }
 func main() {
 	//Init Router
 
 	r := mux.NewRouter()
 
+	// Mock Data - @todo - implement DB
+	books = append(books, Book{ID: "1", Isbn: "448743", Title: "Book One", Author: &Author{FirstName: "John", LastName: "Doe"}})
+	books = append(books, Book{ID: "2", Isbn: "888743", Title: "Book Two", Author: &Author{FirstName: "Streve", LastName: "Smith"}})
 	// Route Handlers / Endpoints
-
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
 	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
 	r.HandleFunc("/api/books", createBook).Methods("POST")
